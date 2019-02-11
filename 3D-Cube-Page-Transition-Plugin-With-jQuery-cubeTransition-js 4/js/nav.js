deabc9239de2615c1bc2704f86fe911fa8392551
VAFE 360 <vafe360@MacBook-Pro-de-VAFE.local>
2019-02-11 13:41:21 +0100

    Test

---
+++ 3D-Cube-Page-Transition-Plugin-With-jQuery-cubeTransition-js 4/js/nav.js
@@ -0,0 +1,9 @@
+
+<script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+	$("#contact").click(function(){
+		$("#page3").addClass("visible");
+	});
+});
+</script>
